3056092ee5998830693da2ff727f233e68e5b842
Alexis Panyathong <alexispanyathong@gmail.com>
2019-10-14 23:19:34 -0500

    Added my middle name to my About Page.

--- alexis-vilai-photography/src/components/AboutPage.js
+++ alexis-vilai-photography/src/components/AboutPage.js
@@ -12,7 +12,7 @@ const AboutPage = () => {
             <About>
 
                 <div className="middle-content">
-                    <p>     Hi I'm Alexis Panyathong. I have always had 
+                    <p>     Hi I'm Alexis Vilai Panyathong. I have always had 
                         a passion for photography. People started noticing my work and loved it
                         and encourage me to get into photography classes, but I was afraid.
                         In April 2017 my fiance at the time surprised me with a gift that changed our lives, a Canon Rebel EOS t2i with two set of lens and told me that I can finally take the next steps on starting my own
