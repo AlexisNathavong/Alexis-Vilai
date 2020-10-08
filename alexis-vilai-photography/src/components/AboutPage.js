985b092a2b48c459b1e3b3751f9e97d99e3d9ef2
Alexis <alexispanyathong@gmail.com>
2020-10-07 20:53:46 -0500

    Updates in About Page.

--- alexis-vilai-photography/src/components/AboutPage.js
+++ alexis-vilai-photography/src/components/AboutPage.js
@@ -26,14 +26,7 @@ const AboutPage = () => {
             {/* <About> */}
 
                 <div className="middle-content">
-                    {/* <p>     Hi I'm Alexis Vilai Panyathong. I have always had 
-                        a passion for photography. People started noticing my work and loved it
-                        and encourage me to get into photography classes, but I was afraid.
-                        In April 2017 my fiance at the time surprised me with a gift that changed our lives, a Canon Rebel EOS t2i with two set of lens and told me that I can finally take the next steps on starting my own
-                        photography business. 
-
-                    </p> */}
-                {/* <img className="img-container" src={require('..//imgs/IMG_9272-WM-resize.jpg')} alt="img of Alexis." /> */}
+                  
                 
                 </div>
             {/* </About> */}
